3af02f430586fe1826cc7bd788cb5bc36d782f45
Julian <122035543+Jg-Stevan@users.noreply.github.com>
2023-01-05 18:44:28 -0500

    avance

---
+++ typesjs.js
@@ -0,0 +1,30 @@
+// TIPOS PRIMITIVOS
+
+//NUMBER
+4;
+
+//STRING
+
+"Hola mundo";
+'hola mundo';
+
+//BOOLEAN
+
+true;
+false;
+
+// NULL AND UNDEFINED
+
+null; //espacio que esta definido en memoria pero esta vacion
+undefined; //no esta definido en memoria y esta vacio
+
+
+if () {
+    console.log("cumple");
+    
+}
+else {
+    console.log("no cumple");
+
+}
+
---
+++ variables.js
@@ -0,0 +1,24 @@
+// como declaramos variables?
+
+var variables;
+let variablelet;
+
+// const constante;
+
+var a = 1;
+console.log(a)
+
+var a = "hola perre verge";
+
+console.log(a)
+
+var a = 3;
+
+console.log(a);
+const hola = "soy julian"
+
+console.log(hola);
+
+const adios = "bye soy julian";
+
+console.log(adios)
